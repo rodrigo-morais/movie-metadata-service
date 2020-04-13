f52996b6e0a13c2d61edc2f169b21c0e6198a406
Rodrigo Moreira de Morais <morais.rm@gmail.com>
2020-04-13 12:01:34 -0300

    Improving endpoints to return the correct status code.

--- src/api/routes.js
+++ src/api/routes.js
@@ -6,11 +6,26 @@ router
   .get('/', async (req, res) => {
     const searchKey = Object.keys(req.query)[0] || null
     const searchValue = req.query[Object.keys(req.query)[0]] || null
+    const movies = await searchMovies(searchKey, searchValue)
 
-    res.send(await searchMovies(searchKey, searchValue))
+    if(!!movies) {
+      res.status(200)
+    } else {
+      res.status(404)
+    }
+
+    res.send(movies)
   })
-  .get('/:id', async (req, res) =>
-    res.send(await getMovieById(req.params.id))
-  )
+  .get('/:id', async (req, res) => {
+    const movie = await getMovieById(req.params.id)
+
+    if(!!movie) {
+      res.status(200)
+    } else {
+      res.status(404)
+    }
+    
+    res.send(movie)
+  })
 
 module.exports = router
